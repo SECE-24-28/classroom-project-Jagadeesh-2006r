2d93feb97b9d3d22525326dec8f7ea035d2d41d9
Jagadeesh-2006r <jagadeesh2006r@gmail.com>
2025-12-04 19:00:02 +0530

    Delete Data_Jpa directory

--- Data_Jpa/new/receiptcollector/src/main/java/com/example/receiptcollector/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.receiptcollector.repository;
-
-
-import com.example.receiptcollector.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    boolean existsByEmail(String email);
-}
