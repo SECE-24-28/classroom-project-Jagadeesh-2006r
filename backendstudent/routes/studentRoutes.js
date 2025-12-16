f661bd9b7416e57763ca01e34955e0b7ef5c3904
Dhushyanthan <dhushyanthan12b@gmail.com>
2025-12-16 19:25:00 +0530

    13th day commit

---
+++ backendstudent/routes/studentRoutes.js
@@ -0,0 +1,26 @@
+const express =require("express")
+const {
+    createStudent,
+    getAllStudents,
+    getStudent,deleteStudent,updateStudent} 
+=require("../controllers/studentController");
+
+
+const router=express.Router();
+router.post("/",createStudent) ;
+router.get("/hello",getAllStudents);
+router.get("/:rollno",getStudent);
+router.delete("/:rollno",deleteStudent)
+router.put("/:rollno",updateStudent);
+
+module.exports=router;
+
+
+
+
+
+
+
+
+
+
